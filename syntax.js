5294f2aabda81b52782dc434229e4628c90abe15
sophie.chhoeu <sophie.chhoeu@carsguide.com.au>
2018-03-21 16:10:04 +1100

    problem example

--- syntax.js
+++ syntax.js
@@ -192,3 +192,48 @@ console.log(c);
 // * **Bonus Challenge:** Try to incorporate input into this program, perhaps with the `prompt(..)` covered in "Input" earlier. You may prompt the user for their bank account balance, for example. Have fun and be creative!
 //
 // OK, go ahead. Try it. Don't peek at my code listing until you've given it a shot yourself!
+
+const SPENDING_THRESHOLD = 200;
+const TAX_RATE = 0.08;
+const PHONE_PRICE = 99.99;
+const ACCESSORY_PRICE = 9.99;
+
+var bank_balance = 303.91;
+var amount = 0;
+
+function calculateTax(amount) {
+    return amount * TAX_RATE;
+}
+
+function formatAmount(amount) {
+    return "$" + amount.toFixed( 2 );
+}
+
+// keep buying phones while you still have money
+while (amount < bank_balance) {
+    // buy a new phone!
+    amount = amount + PHONE_PRICE;
+
+    // can we afford the accessory?
+    if (amount < SPENDING_THRESHOLD) {
+        amount = amount + ACCESSORY_PRICE;
+    }
+}
+
+// don't forget to pay the government, too
+amount = amount + calculateTax( amount );
+
+console.log(
+    "Your purchase: " + formatAmount( amount )
+);
+// Your purchase: $334.76
+
+// can you actually afford this purchase?
+if (amount > bank_balance) {
+    console.log(
+        "You can't afford this purchase. :("
+    );
+}
+
+//es6
+//utilise babel (that transpiles to the browser)
